2d359de667462001a5873375aa7b727da5321f79
Lucas Freitas <lucas.freitas@stuo.app>
2023-08-24 01:48:11 -0300

    Update ormconfig Implement TypeOrmModule in app.module.ts Connect with dockerized db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,31 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AddressesModule } from './addresses/addresses.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ContactsModule } from './contacts/contacts.module';
+import { EmailsModule } from './emails/emails.module';
+import { PhonesModule } from './phones/phones.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      migrationsTableName: 'migrations',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      migrations: [__dirname + '/migration/**/*{.ts,.js}'],
+      synchronize: true,
+      migrationsRun: true,
+      logging: false,
+    }),
+    ContactsModule,
+    PhonesModule,
+    EmailsModule,
+    AddressesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,18 +1,18 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
 
-const config: DataSourceOptions = {
+const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
   host: process.env.DB_HOST || 'localhost',
-  port: process.env.DB_PORT ? +process.env.DB_PORT : 5010,
+  port: process.env.DB_PORT ? +process.env.DB_PORT : 5432,
   username: process.env.DB_USERNAME || 'contatinhouser',
   password: process.env.DB_PASS || 'ultramegasecret',
   database: process.env.DB_DATABASE || 'contatinho',
   migrationsTableName: 'migrations',
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
   migrations: [__dirname + '/migration/**/*{.ts,.js}'],
-  synchronize: false,
+  synchronize: true,
   migrationsRun: true,
   logging: false,
 };
 
-export default new DataSource(config);
+export default new DataSource(dataSourceOptions);
